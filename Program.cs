b4fd92f290a987cdcab3b972e5e47c124d5778b6
LautaroLopez2 <lautalopez2448@gmail.com>
2022-10-12 21:24:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tp_6_eje_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ponga el agua a calentar");
+            Console.ReadKey();
+            Console.WriteLine("Muela el cafe");
+            Console.ReadKey();
+            Console.WriteLine("Poner en una taza junto al agua ya caliente");
+            Console.ReadKey();
+            Console.WriteLine("ponerle azucar al gusto");
+            Console.ReadKey();
+            Console.WriteLine("Disfruste su Cafe");
+            Console.ReadKey();
+
+        }
+    }
+}
